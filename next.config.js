3b2cc51d86c1eebe3e4eb4176cea4fa2a1e08533
nguyenminhphuc15 <nguyenphucminh880@gmail.com>
2025-03-02 21:37:53 +0700

    Cập nhật cấu hình Next.js và build lại

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
-const path = require('path')
- 
-module.exports = {
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
+    domains: [
+      "media.dev.to",
+      "avatars.githubusercontent.com",
+      "github-readme-stats.vercel.app",
+      "github-readme-streak-stats.herokuapp.com",
+      "stardev.io",
+      "github-profile-summary-cards.vercel.app",
+      "github-profile-trophy.vercel.app",
+    ],
     dangerouslyAllowSVG: true,
-  }
-}
+  },
+  output: "export", // Add this for static export
+};
+
+module.exports = nextConfig;
