a700270a53e468259f89f7445725273ae4915625
nguyenminhphuc15 <nguyenphucminh880@gmail.com>
2025-03-02 21:52:33 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
-const path = require("path");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const path = require('path')
+ 
+module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: [
-      "media.dev.to",
-      "avatars.githubusercontent.com",
-      "github-readme-stats.vercel.app",
-      "github-readme-streak-stats.herokuapp.com",
-      "stardev.io",
-      "github-profile-summary-cards.vercel.app",
-      "github-profile-trophy.vercel.app",
-    ],
+    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
     dangerouslyAllowSVG: true,
-  },
-  output: "export", // Add this for static export
-};
-
-module.exports = nextConfig;
+  }
+}
