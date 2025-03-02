a98d9107f532304a321a01d5a4cdf961abd173c6
nguyenminhphuc15 <nguyenphucminh880@gmail.com>
2025-03-02 20:50:01 +0700

    Done 2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
-const path = require('path')
- 
-module.exports = {
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
+    domains: [
+      "media.dev.to",
+      "avatars.githubusercontent.com",
+      "github-readme-stats.vercel.app",
+      "github-readme-streak-stats.herokuapp.com",
+      "stardev.io",
+      "github-profile-summary-cards.vercel.app",
+      "github-profile-trophy.vercel.app",
+    ],
     dangerouslyAllowSVG: true,
-  }
-}
+  },
+  output: "export", // Thêm dòng này để xuất trang tĩnh
+  trailingSlash: true, // Tùy chọn: Thêm dấu gạch chéo vào cuối tất cả các đường dẫn, ví dụ: `/about` -> `/about/`
+};
+
+module.exports = nextConfig;
