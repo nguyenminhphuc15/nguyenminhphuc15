94037c7c5c272e1b5489b7ccc5d4112d2cddc6a8
nguyenminhphuc15 <nguyenphucminh880@gmail.com>
2025-03-02 20:29:38 +0700

    Done

---
+++ data/user-data.js
@@ -0,0 +1,20 @@
+export const userData = {
+  githubUser: "nguyenminhphuc15",
+  devUsername: "nguyenminhphuc15",
+  github: "https://github.com/nguyenminhphuc15",
+  facebook: "https://www.facebook.com/nguyen.phuc.191857/",
+  resume:
+    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  skills: [
+    "React",
+    "NextJS",
+    "Redux",
+    "Express",
+    "MySql",
+    "MongoDB",
+    "Postgres",
+    "Docker",
+    "AWS",
+  ],
+  timezone: "+6",
+};
